20e28a690010ecfc19d821461d23e05e35bbd3f2
Eshwar Tangirala <eshwartangirala11@gmail.com>
2022-12-06 20:42:51 -0500

    first commiddsst

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -2,7 +2,7 @@ import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Footer = () => {
   return (
-    <footer className="h-20 bg-red pt-10">
+    <footer className="h-20 bg-black">
       <div className="w-10/12 mx-auto">
         <div className="md:flex justify-center md:justify-between text-center ">
           <p className="font-playfair font-semibold text-2xl text-yellow">
